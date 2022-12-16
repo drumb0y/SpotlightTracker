c593a73417fd8194bd7b2c353d19b856a3946161
drumb <drumb0y234@gmail.com>
2022-12-15 21:37:59 -0600

    worked on OSC Communication

--- src/OSC/OSCSender.java
+++ src/OSC/OSCSender.java
@@ -9,14 +9,17 @@ import java.net.UnknownHostException;
 import java.util.Scanner;
 
 public class OSCSender {
-    public static void main(String[] args) throws UnknownHostException, IOException, OSCSerializeException {
-
+    public static void main(String[] args) throws UnknownHostException, IOException, OSCSerializeException, InterruptedException {
+//169.254.249.205 - wired
+        //192.168.0.113 - wifi
         Scanner stdin = new Scanner(System.in);
         System.out.println("Print Receivers ip address");
+        int count = 1;
 
         String ipaddress = stdin.nextLine();
-        OSCPortOut sender = new OSCPortOut(InetAddress.getByName(ipaddress), 53001);
-        OSCMessage msg = new OSCMessage("/cue/1");
+        OSCPortOut sender = new OSCPortOut(InetAddress.getByName(ipaddress), 9998);
+        OSCMessage msg = new OSCMessage("/cue/" + count);
+
         try {
             sender.send(msg);
         } catch (Exception e) {
@@ -24,7 +27,8 @@ public class OSCSender {
         }
 
         while (true) {
-            sender.send(msg);
+            Thread.sleep(100);
+            sender.send(new OSCMessage("/cue/" + count++));
         }
     }
 }
