7c0a0f49c8ed6b0f78cd34234a054d8c42884cf0
drumb <drumb0y234@gmail.com>
2022-12-18 20:40:08 -0600

    worked on OSC Communication

--- src/OSC/OSCReceiver.java
+++ src/OSC/OSCReceiver.java
@@ -1,8 +1,6 @@
 package OSC;
 
-import com.illposed.osc.OSCBadDataEvent;
-import com.illposed.osc.OSCPacketEvent;
-import com.illposed.osc.OSCPacketListener;
+import com.illposed.osc.*;
 import com.illposed.osc.transport.udp.OSCPort;
 import com.illposed.osc.transport.udp.OSCPortIn;
 
@@ -10,20 +8,18 @@ import java.io.IOException;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.SocketAddress;
-import java.net.UnknownHostException;
-import java.util.Scanner;
 
 public class OSCReceiver extends OSCPort{
     protected OSCReceiver(SocketAddress local, SocketAddress remote) throws IOException {
         super(local, remote);
     }
 
-    public static void main(String[] args) throws IOException {
-        int port = 53000;
+    public static void main(String[] args) throws IOException, InterruptedException {
+        int port = 9999;
         System.out.println(InetAddress.getLocalHost());
         String fullAddress = InetAddress.getLocalHost().toString();
-        String IPAddress = fullAddress.substring(fullAddress.indexOf("/")+1);
-        System.out.println(IPAddress);
+        String outsideIPAddress = fullAddress.substring(fullAddress.indexOf("/")+1);
+        System.out.println(outsideIPAddress);
         System.out.println();
 
         //Scanner stdin = new Scanner(System.in);
@@ -31,16 +27,18 @@ public class OSCReceiver extends OSCPort{
 
         //String RemoteIpaddress = stdin.nextLine();
 
-        SocketAddress localSocket = new InetSocketAddress(IPAddress, 53001);
+        SocketAddress localSocket = new InetSocketAddress(outsideIPAddress, port);
         //SocketAddress remoteSocket = new InetSocketAddress(RemoteIpaddress, 53000);
 
-        OSCPortIn receiver = new OSCPortIn(53001);
+        OSCPortIn receiver = new OSCPortIn(port);
+        receiver.startListening();
 
-        receiver.addPacketListener(new messageListener());
+        receiver.addPacketListener(new packetListener());
 
         while (true) {
+            Thread.sleep(100);
 
-            System.out.println(receiver.getPacketListeners());
+            //System.out.println(receiver.getPacketListeners());
 
 
         }
@@ -51,11 +49,24 @@ public class OSCReceiver extends OSCPort{
 
 
 }
-class messageListener implements OSCPacketListener {
+
+class messageListener implements OSCMessageListener {
+
+    @Override
+    public void acceptMessage(OSCMessageEvent oscMessageEvent) {
+        OSCMessage message = oscMessageEvent.getMessage();
+
+        System.out.println("Message recived: " + message.toString());
+    }
+}
+class packetListener implements OSCPacketListener {
 
     @Override
     public void handlePacket(OSCPacketEvent oscPacketEvent) {
-        System.out.println("IT WORKED " + oscPacketEvent);
+        OSCMessage msg = (OSCMessage) oscPacketEvent.getPacket();
+
+        System.out.printf("%s%s\n","IT WORKED ", msg.getAddress());
+
     }
 
     @Override
